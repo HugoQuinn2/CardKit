d9703379b8f8caf8e29ce3365d606aed0174c2f5
vgaspar <hugoquinn2@gmail.com>
2025-10-06 16:35:38 -0600

    refactor code

--- cardkit-calypso/src/main/java/com/idear/devices/card/cardkit/calypso/transaction/ReadAllCardData.java
+++ cardkit-calypso/src/main/java/com/idear/devices/card/cardkit/calypso/transaction/ReadAllCardData.java
@@ -10,6 +10,7 @@ import com.idear.devices.card.cardkit.calypso.ReaderPCSC;
 import com.idear.devices.card.cardkit.calypso.file.*;
 import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
+import lombok.var;
 import org.eclipse.keypop.calypso.card.WriteAccessLevel;
 import org.eclipse.keypop.calypso.card.card.CalypsoCard;
 import org.eclipse.keypop.calypso.card.card.SvDebitLogRecord;
@@ -19,6 +20,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedMap;
 
+/**
+ * Reads all Calypso card data and maps it to {@link CalypsoCardCDMX}.
+ * <p>
+ * This transaction performs:
+ * <ul>
+ *     <li>A simple read to retrieve basic information.</li>
+ *     <li>Reading of environment, events, contracts, and log files.</li>
+ *     <li>Parsing of all file contents into structured domain models.</li>
+ * </ul>
+ * </p>
+ *
+ * @author Victor Hugo Gaspar Quinn
+ */
 @EqualsAndHashCode(callSuper = true)
 @Slf4j
 public class ReadAllCardData extends Transaction<CalypsoCardCDMX, ReaderPCSC> {
@@ -27,7 +41,8 @@ public class ReadAllCardData extends Transaction<CalypsoCardCDMX, ReaderPCSC> {
 
     private CalypsoCardCDMX cdmxCard;
     private TransactionResult<CalypsoCardCDMX> simpleRead;
-    private TransactionResult<CalypsoCard> readFile;
+    private TransactionResult<byte[]> readFile;
+    private TransactionResult<SortedMap<Integer, byte[]>> readFiles;
 
     /**
      * Create an {@link ReadAllCardData} transaction, this transaction read all {@link CalypsoCard} data and parse
@@ -51,96 +66,87 @@ public class ReadAllCardData extends Transaction<CalypsoCardCDMX, ReaderPCSC> {
 
         simpleRead = reader.executeTransaction(new SimpleReadCard());
 
-        if (!simpleRead.is(TransactionStatus.OK))
+        if (!simpleRead.isOk())
             throw new ReaderException(simpleRead.getMessage());
 
         cdmxCard = simpleRead.getData();
-        log.info("Reading all card data '{}'", cdmxCard.getSerial());
+        log.info("Reading all card data for '{}'", cdmxCard.getSerial());
 
-        // Read environment file (0x07) and parse
-        readFile = reader.executeTransaction(
-                new ReadCardFile(writeAccessLevel, CDMX.ENVIRONMENT_FILE, 1)
-        );
-
-        cdmxCard.setBalance(reader.getCalypsoCard().getSvBalance());
-
-        if (readFile.is(TransactionStatus.OK)) {
-            cdmxCard.setEnvironment(
-                    new Environment(
-                            readFile.getData()
-                                    .getFileBySfi(CDMX.ENVIRONMENT_FILE)
-                                    .getData()
-                                    .getContent()
-                    )
-            );
-        } else {
-            log.warn("The environment file could not be read: {}", readFile.getMessage());
-        }
-
-        // Read debit log file (0x15) and parse
-        SvDebitLogRecord svDebitLogRecord = reader.getCalypsoCard().getSvDebitLogLastRecord();
-        if (svDebitLogRecord != null) {
-            cdmxCard.setDebitLog(new DebitLog(svDebitLogRecord));
-        } else {
-            log.warn("The debit log file could not be read");
-        }
-
-        // Read load log file (0x14) and parse
-        SvLoadLogRecord svLoadLogRecord = reader.getCalypsoCard().getSvLoadLogRecord();
-        if (svDebitLogRecord != null) {
-            cdmxCard.setLoadLog(new LoadLog(svLoadLogRecord));
-        } else {
-            log.warn("The load log file could not be read");
-        }
-
-
-        // Read events log file (0x08) and parse
-        readFile = reader.executeTransaction(
-                new ReadCardFilePartially(CDMX.EVENT_FILE, (byte) 1, (byte) 3,0, 29)
-        );
-
-        if (readFile.is(TransactionStatus.OK)) {
-            List<Event> events = new ArrayList<>();
-            SortedMap<Integer, byte[]> partialEvents = readFile.getData()
-                    .getFileBySfi(CDMX.EVENT_FILE)
-                    .getData()
-                    .getAllRecordsContent();
-
-            for (int id : partialEvents.keySet()) {
-                events.add(new Event(id, partialEvents.get(id)));
-            }
-
-            cdmxCard.setEvents(events);
-        } else {
-            log.warn("The events file could not be read: {}", readFile.getMessage());
-        }
-
-        // Read contracts and parse (0x09)
-        readFile = reader.executeTransaction(
-                new ReadCardFilePartially((byte) 0x09, (byte) 1, (byte) 8, 0, 10)
-        );
-
-        if (readFile.is(TransactionStatus.OK)) {
-            Contracts contracts = new Contracts();
-
-            SortedMap<Integer, byte[]> partialContracts = readFile.getData()
-                    .getFileBySfi((byte) 0x09)
-                    .getData()
-                    .getAllRecordsContent();
-
-            for (int id : partialContracts.keySet()) {
-                contracts.add(new Contract(id, partialContracts.get(id)));
-            }
-
-            cdmxCard.setContracts(contracts);
-        } else {
-            log.warn("The contracts file could not be read: {}", readFile.getMessage());
-        }
+        readEnvironmentFile(reader);
+        readLogFiles(reader);
+        readEventFiles(reader);
+        readContractFiles(reader);
 
         return TransactionResult
                 .<CalypsoCardCDMX>builder()
                 .transactionStatus(TransactionStatus.OK)
                 .data(cdmxCard)
                 .build();
+    }
+
+    private void readEnvironmentFile(ReaderPCSC reader) {
+        readFile = reader.executeTransaction(
+                new ReadCardFile(writeAccessLevel, CDMX.ENVIRONMENT_FILE, 1)
+        );
+
+        cdmxCard.setBalance(reader.getCalypsoCard().getSvBalance());
+
+        if (readFile.isOk()) {
+            cdmxCard.setEnvironment(new Environment(readFile.getData()));
+        } else {
+            log.warn("Failed to read environment file: {}", readFile.getMessage());
+        }
+    }
+
+    private void readLogFiles(ReaderPCSC reader) {
+        SvDebitLogRecord debitLogRecord = reader.getCalypsoCard().getSvDebitLogLastRecord();
+        if (debitLogRecord != null) {
+            cdmxCard.setDebitLog(new DebitLog(debitLogRecord));
+        } else {
+            log.warn("Failed to read debit log record");
+        }
+
+        SvLoadLogRecord loadLogRecord = reader.getCalypsoCard().getSvLoadLogRecord();
+        if (loadLogRecord != null) {
+            cdmxCard.setLoadLog(new LoadLog(loadLogRecord));
+        } else {
+            log.warn("Failed to read load log record");
+        }
+    }
+
+    private void readEventFiles(ReaderPCSC reader) {
+        readFiles = reader.executeTransaction(
+                new ReadCardFilePartially(CDMX.EVENT_FILE, (byte) 1, (byte) 3, 0, 29)
+        );
+
+        if (!readFiles.isOk()) {
+            log.warn("Failed to read events file: {}", readFiles.getMessage());
+            return;
+        }
+
+        List<Event> events = new ArrayList<>();
+        SortedMap<Integer, byte[]> partialEvents = readFiles.getData();
+        for (var entry : partialEvents.entrySet()) {
+            events.add(new Event(entry.getKey(), entry.getValue()));
+        }
+        cdmxCard.setEvents(events);
+    }
+
+    private void readContractFiles(ReaderPCSC reader) {
+        readFiles = reader.executeTransaction(
+                new ReadCardFilePartially((byte) 0x09, (byte) 1, (byte) 8, 0, 10)
+        );
+
+        if (!readFiles.isOk()) {
+            log.warn("Failed to read contracts file: {}", readFiles.getMessage());
+            return;
+        }
+
+        Contracts contracts = new Contracts();
+        SortedMap<Integer, byte[]> partialContracts = readFiles.getData();
+        for (var entry : partialContracts.entrySet()) {
+            contracts.add(new Contract(entry.getKey(), entry.getValue()));
+        }
+        cdmxCard.setContracts(contracts);
     }
 }
