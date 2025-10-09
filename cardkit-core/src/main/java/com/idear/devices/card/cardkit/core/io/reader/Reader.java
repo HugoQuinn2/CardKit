3711a2d3b74f6f941cdaae30270724a7fa3dde26
hugoq <hugoquinn2@gmail.com>
2025-10-09 13:43:14 -0600

    ✨ feature(transaction): add auto ABORTED status on exceptions card kit, and optional progress listeners to Transaction.java

--- cardkit-core/src/main/java/com/idear/devices/card/cardkit/core/io/reader/Reader.java
+++ cardkit-core/src/main/java/com/idear/devices/card/cardkit/core/io/reader/Reader.java
@@ -1,5 +1,8 @@
 package com.idear.devices.card.cardkit.core.io.reader;
 
+import com.idear.devices.card.cardkit.core.exception.CardException;
+import com.idear.devices.card.cardkit.core.exception.ReaderException;
+import com.idear.devices.card.cardkit.core.exception.SamException;
 import com.idear.devices.card.cardkit.core.io.transaction.Transaction;
 import com.idear.devices.card.cardkit.core.io.transaction.TransactionResult;
 import com.idear.devices.card.cardkit.core.io.transaction.TransactionStatus;
@@ -21,15 +24,25 @@ public abstract class Reader<E> {
      * @return the result transaction
      */
     public <T, R extends Reader<E>> TransactionResult<T> execute(Transaction<T, R> transaction) {
+        long time = System.currentTimeMillis();
         try {
-            long time = System.currentTimeMillis();
             TransactionResult<T> transactionResult = transaction.execute((R) this);
             transactionResult.setTime(System.currentTimeMillis() - time);
+            transactionResult.setTransactionName(transaction.getName());
             return transactionResult;
+        } catch (CardException | SamException | ReaderException aborted) {
+            return TransactionResult.<T>builder()
+                    .transactionStatus(TransactionStatus.ABORTED)
+                    .transactionName(transaction.getName())
+                    .message( aborted.getClass().getSimpleName() + ": " + aborted.getMessage())
+                    .time(System.currentTimeMillis() - time)
+                    .build();
         } catch (Exception e) {
             return TransactionResult.<T>builder()
                     .transactionStatus(TransactionStatus.ERROR)
+                    .transactionName(transaction.getName())
                     .message(e.getMessage())
+                    .time(System.currentTimeMillis() - time)
                     .build();
         }
     }
