d33071d0b75b6b2e143e5c5d44982dce61305677
hugoq <hugoquinn2@gmail.com>
2025-10-27 10:37:40 -0600

    se implementa CardProperty en Event.java

--- cardkit-calypso/src/main/java/com/idear/devices/card/cardkit/calypso/file/Event.java
+++ cardkit-calypso/src/main/java/com/idear/devices/card/cardkit/calypso/file/Event.java
@@ -3,6 +3,7 @@ package com.idear.devices.card.cardkit.calypso.file;
 import com.idear.devices.card.cardkit.core.datamodel.calypso.*;
 import com.idear.devices.card.cardkit.core.datamodel.date.DateTimeReal;
 import com.idear.devices.card.cardkit.core.datamodel.location.LocationCode;
+import com.idear.devices.card.cardkit.core.io.card.cardProperty.CardProperty;
 import com.idear.devices.card.cardkit.core.io.card.file.File;
 import com.idear.devices.card.cardkit.core.utils.BitUtil;
 import com.idear.devices.card.cardkit.core.utils.ByteUtils;
@@ -16,16 +17,16 @@ public class Event extends File<Event> {
 
     private int id;
 
-    private Version version;
+    private final CardProperty<Version> version = CardProperty.emptyProperty(Version.class);
     private int transactionNumber;
-    private TransactionType transactionType;
-    private NetworkCode networkId;
-    private Provider provider;
-    private LocationCode locationId;
+    private final CardProperty<TransactionType> transactionType = CardProperty.emptyProperty(TransactionType.class);
+    private final CardProperty<NetworkCode> networkId = CardProperty.emptyProperty(NetworkCode.class);
+    private final CardProperty<Provider> provider = CardProperty.emptyProperty(Provider.class);
+    private final LocationCode locationId = LocationCode.emptyLocationCode();
     private DateTimeReal dateTimeStamp;
     private int amount;
-    private Provider firstServiceProvider;
-    private LocationCode firstLocationId;
+    private final CardProperty<Provider> firstServiceProvider = CardProperty.emptyProperty(Provider.class);
+    private final LocationCode firstLocationId = LocationCode.emptyLocationCode();
     private DateTimeReal firstDateTimeStamp;
     private int firstPassenger;
     private int firstContractsUsed;
@@ -61,16 +62,16 @@ public class Event extends File<Event> {
 
     @Override
     public Event parse(byte[] data) {
-        this.version              = Version.decode(data[0] & 0xff);
+        this.version.setValue(data[0] & 0xff);
         this.transactionNumber    = ByteUtils.extractInt(data, 1, 3, false);
-        this.transactionType      = TransactionType.decode(data[4] & 0xff);
-        this.networkId            = NetworkCode.decode(data[5] & 0xff);
-        this.provider             = Provider.decode(data[6] & 0xff);
-        this.locationId           = new LocationCode(ByteUtils.extractInt(data, 7, 3, false));
+        this.transactionType.setValue(data[4] & 0xff);
+        this.networkId.setValue(data[5] & 0xff);
+        this.provider.setValue(data[6] & 0xff);
+        this.locationId.setValue(ByteUtils.extractInt(data, 7, 3, false));
         this.dateTimeStamp        = DateTimeReal.fromSeconds(ByteUtils.extractInt(data, 10, 4, false));
         this.amount               = ByteUtils.extractInt(data, 14, 3, false);
-        this.firstServiceProvider = Provider.decode(data[17] & 0xff);
-        this.firstLocationId      = new LocationCode(ByteUtils.extractInt(data, 18, 3, false));
+        this.firstServiceProvider.setValue(data[17] & 0xff);
+        this.firstLocationId.setValue(ByteUtils.extractInt(data, 18, 3, false));
         this.firstDateTimeStamp   = DateTimeReal.fromSeconds(ByteUtils.extractInt(data, 21, 4, false));
         this.firstPassenger       = data[25] & 0xff;
         this.firstContractsUsed   = data[26] & 0xff;
@@ -81,29 +82,29 @@ public class Event extends File<Event> {
     }
 
     public static Event builEvent(
-            TransactionType transactionType,
-            NetworkCode networkCode,
-            Provider provider,
+            int transactionType,
+            int networkCode,
+            int provider,
             int contractId,
             int passenger,
             int transactionNumber,
-            LocationCode locationId,
+            int locationId,
             int amount) {
         Event event = new Event(1);
 
-        event.setTransactionType(transactionType);
-        event.setNetworkId(networkCode);
+        event.getTransactionType().setValue(transactionType);
+        event.getNetworkId().setValue(networkCode);
 
         event.setTransactionNumber(transactionNumber);
-        event.setLocationId(locationId);
+        event.getLocationId().setValue(locationId);
         event.setAmount(amount);
 
-        event.setVersion(Version.VERSION_3_3);
-        event.setProvider(provider);
+        event.getVersion().setValueByModel(Version.VERSION_3_3);
+        event.getProvider().setValue(provider);
         event.setDateTimeStamp(DateTimeReal.now());
         event.setFirstDateTimeStamp(DateTimeReal.now());
-        event.setFirstServiceProvider(provider);
-        event.setFirstLocationId(locationId);
+        event.getFirstServiceProvider().setValue(provider);
+        event.getFirstLocationId().setValue(locationId);
         event.setFirstContractsUsed(getContractBitmap(contractId));
         event.setFirstPassenger(passenger);
 
