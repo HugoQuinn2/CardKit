4feeacced28bbbb8df4e51d2c09e393b91f49b06
vgaspar <hugoquinn2@gmail.com>
2025-12-18 15:18:39 -0600

    se agrega objeto para agregar logs de debito y recarga

---
+++ cardkit-core/src/main/java/com/idear/devices/card/cardkit/core/datamodel/calypso/file/Logs.java
@@ -0,0 +1,9 @@
+package com.idear.devices.card.cardkit.core.datamodel.calypso.file;
+
+import lombok.Data;
+
+@Data
+public class Logs {
+    private DebitLog debitLog;
+    private LoadLog loadLog;
+}
