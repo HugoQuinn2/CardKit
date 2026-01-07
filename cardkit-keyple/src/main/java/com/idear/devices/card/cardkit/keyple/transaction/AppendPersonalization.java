aecdeccee2688fcb1fbaf276a6b0c65b6c7d149d
vgaspar <hugoquinn2@gmail.com>
2026-01-07 11:25:38 -0600

    se genera transaccion de personalizacion append

---
+++ cardkit-keyple/src/main/java/com/idear/devices/card/cardkit/keyple/transaction/AppendPersonalization.java
@@ -0,0 +1,35 @@
+package com.idear.devices.card.cardkit.keyple.transaction;
+
+import com.idear.devices.card.cardkit.core.io.transaction.AbstractTransaction;
+import com.idear.devices.card.cardkit.core.io.transaction.TransactionResult;
+import com.idear.devices.card.cardkit.keyple.KeypleTransactionContext;
+import com.idear.devices.card.cardkit.keyple.KeypleUtil;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.eclipse.keypop.calypso.card.WriteAccessLevel;
+import org.eclipse.keypop.calypso.card.transaction.ChannelControl;
+
+@RequiredArgsConstructor
+@Getter
+public class AppendPersonalization extends AbstractTransaction<Boolean, KeypleTransactionContext> {
+
+    private final byte fileId;
+    private final byte[] data;
+
+    @Override
+    public TransactionResult<Boolean> execute(KeypleTransactionContext context) {
+
+        KeypleUtil.appendEditCardFile(
+                context.getCardTransactionManager(),
+                WriteAccessLevel.PERSONALIZATION,
+                fileId,
+                data,
+                ChannelControl.CLOSE_AFTER
+        );
+
+        return TransactionResult
+                .<Boolean>builder()
+                .data(true)
+                .build();
+    }
+}
