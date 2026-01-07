fe2823d4c836b8e643095456be56fa0b6f588f75
vgaspar <hugoquinn2@gmail.com>
2026-01-07 11:21:02 -0600

    se simplifica las personalizacion para mas libertal al usuario

--- cardkit-keyple/src/main/java/com/idear/devices/card/cardkit/keyple/transaction/Personalization.java
+++ cardkit-keyple/src/main/java/com/idear/devices/card/cardkit/keyple/transaction/Personalization.java
@@ -6,23 +6,29 @@ import com.idear.devices.card.cardkit.core.io.card.file.File;
 import com.idear.devices.card.cardkit.core.io.transaction.TransactionResult;
 import com.idear.devices.card.cardkit.keyple.KeypleTransactionContext;
 import com.idear.devices.card.cardkit.keyple.KeypleUtil;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.keypop.calypso.card.WriteAccessLevel;
 import org.eclipse.keypop.calypso.card.transaction.ChannelControl;
 
 @RequiredArgsConstructor
+@Getter
 public class Personalization extends AbstractTransaction<Boolean, KeypleTransactionContext> {
 
+    private final byte fileId;
     private final int recordNumber;
-    private final File<?> file;
+    private final byte[] data;
 
     @Override
     public TransactionResult<Boolean> execute(KeypleTransactionContext context) {
 
-        KeypleUtil.updateRecord(
+        KeypleUtil.editCardFile(
                 context.getCardTransactionManager(),
-                file,
-                recordNumber
+                WriteAccessLevel.PERSONALIZATION,
+                fileId,
+                recordNumber,
+                data,
+                ChannelControl.KEEP_OPEN
         );
 
         return TransactionResult
